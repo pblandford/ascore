08e8e243dc6eec06ae66f19d6a009663e99f4b79
Phil <philip.blandford@gmail.com>
2023-06-07 13:44:23 +0100

    Release 1.1a: Fix crash in error handling

--- ui/src/main/java/org/philblandford/ui/crash/CrashHandler.kt
+++ ui/src/main/java/org/philblandford/ui/crash/CrashHandler.kt
@@ -2,19 +2,18 @@ package org.philblandford.ui.crash
 
 import android.content.Context
 import android.net.Uri
-import com.google.android.gms.common.util.CrashUtils
-import com.google.firebase.crashlytics.internal.common.CrashlyticsCore
-import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.BuildConfig
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
 import com.philblandford.kscore.api.KScore
-import com.philblandford.kscore.saveload.Saver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import org.apache.commons.io.FileUtils
 import org.philblandford.ascore2.features.crosscutting.model.ErrorDescr
 import org.philblandford.ascore2.features.crosscutting.usecases.GetError
@@ -22,11 +21,7 @@ import timber.log.Timber
 import java.io.File
 import java.nio.charset.Charset
 import java.time.LocalDateTime
-import java.time.ZoneId
 import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-import java.time.temporal.TemporalAccessor
 
 class CrashHandler(
   private val getError: GetError, private val kScore: KScore,
@@ -54,28 +49,47 @@ class CrashHandler(
     val storageRef = storage.reference
     val dirRef = storageRef.child("crashed_scores/$timeStamp")
 
-    errorDescr.command?.let {
-      val tmpFile = File(context.cacheDir, "cmd.txt")
-      FileUtils.write(tmpFile, "${BuildConfig.VERSION_NAME}\n${errorDescr.command.toString()}", Charset.defaultCharset())
-      val fileRef = dirRef.child("cmd.txt")
-      coroutineScope.launch(Dispatchers.IO) {
-        fileRef.putFile(Uri.fromFile(tmpFile)).await()
+    coroutineScope.launch {
+      try {
+        withContext(Dispatchers.IO) {
+          uploadCommand(errorDescr, dirRef)
+          uploadException(errorDescr, dirRef)
+          uploadScore(errorDescr, dirRef)
+        }
+      } catch (e:Exception) {
+        Timber.e(e)
       }
     }
-    errorDescr.exception?.let { exception ->
-      val tmpFile = File(context.cacheDir, "exception.txt")
-      FileUtils.write(tmpFile, exception.stackTraceToString(), Charset.defaultCharset())
-      val fileRef = dirRef.child("exception.txt")
-      coroutineScope.launch(Dispatchers.IO) {
-        fileRef.putFile(Uri.fromFile(tmpFile)).await()
-      }
-    }
+  }
 
-    kScore.getScoreAsBytes()?.let { bytes ->
-      coroutineScope.launch(Dispatchers.IO) {
-        val fileRef = dirRef.child("score.asc")
-        fileRef.putBytes(bytes).await()
+  private suspend fun uploadCommand(errorDescr: ErrorDescr, dirRef: StorageReference) {
+    errorDescr.command?.let { command ->
+      val tmpFile = File(context.cacheDir, "cmd.txt")
+      FileUtils.write(
+        tmpFile,
+        "${BuildConfig.VERSION_NAME}\n${errorDescr.command.toString()}",
+        Charset.defaultCharset()
+      )
+      val fileRef = dirRef.child("cmd.txt")
+      fileRef.putFile(Uri.fromFile(tmpFile)).await()
+    }
+  }
+
+  private suspend fun uploadException(errorDescr: ErrorDescr, dirRef: StorageReference) {
+    errorDescr.exception?.let { exception ->
+      withContext(Dispatchers.IO) {
+        val tmpFile = File(context.cacheDir, "exception.txt")
+        FileUtils.write(tmpFile, exception.stackTraceToString(), Charset.defaultCharset())
+        val fileRef = dirRef.child("exception.txt")
+        fileRef.putFile(Uri.fromFile(tmpFile)).await()
       }
+    }
+  }
+
+  private suspend fun uploadScore(errorDescr: ErrorDescr, dirRef: StorageReference) {
+    kScore.getScoreAsBytes()?.let { bytes ->
+      val fileRef = dirRef.child("score.asc")
+      fileRef.putBytes(bytes).await()
     }
   }
 }
