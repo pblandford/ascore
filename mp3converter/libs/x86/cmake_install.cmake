cbc2a545911ed588bb2268af1b6e1518f47e0c17
philblandford <philip.blandford@gmail.com>
2025-03-03 11:08:20 +0000

    Account for ymargin when adding systems

--- mp3converter/libs/x86/cmake_install.cmake
+++ mp3converter/libs/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/phil/Work/ascore2/mp3converter/src/main/cpp/fluidsynth
+# Install script for directory: /Users/philblandford/Work/ascore/mp3converter/src/main/cpp/fluidsynth
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -39,6 +39,6 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/phil/Android/Sdk/ndk/21.3.6528147/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-objdump")
+  set(CMAKE_OBJDUMP "/Users/philblandford/Library/Android/sdk/ndk/21.3.6528147/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-objdump")
 endif()
 
